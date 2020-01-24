3aea9879a51e86742f10d07762895f4960ed18eb
Alfonso Grana <alfonso.granagorlero@asml.com>
2020-01-24 11:02:37 +0000

    reading credentials from env.

--- create_environment/create_env.py
+++ create_environment/create_env.py
@@ -1,13 +1,17 @@
 import boto3
-client = boto3.client('cloud9')
+import os
+# session = boto3.Session(profile_name='devmachina', region_name='us-east-1')
+# Any clients created from this session will use credentials
+# from the [dev] section of ~/.aws/credentials.
+# print(session)
+client = boto3.client('cloud9', region_name='us-east-1', aws_access_key_id=os.environ['ACCESS_KEY'], aws_secret_access_key=os.environ['SECRET_KEY'])
 
 response = client.create_environment_ec2(
         name='devMachina',
         description='0 env cloud9',
-        clientRequestToken='string',
-        instanceType='string',
-        subnetId='string',
-        automaticStopTimeMinutes=30,
-        ownerArn='string'
+        clientRequestToken='alfonsoenv1',
+        instanceType='t3.micro',
+        automaticStopTimeMinutes=20,
+        ownerArn='arn:aws:iam::726747867029:user/alfonso'
         )
 print(response)

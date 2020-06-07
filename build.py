043b105db203ded80959e408b5640e0c8f3b445c
Alfonso Grana <alfonso.grana@gmail.com>
2020-06-07 18:03:42 +0000

    vim and zsh for amazon_linux

---
+++ build.py
@@ -0,0 +1,1 @@
+print("Reached build script")
--- create_environment/create_env.py
+++ create_environment/create_env.py
@@ -1,21 +1,30 @@
 import boto3
 import os
-# session = boto3.Session(profile_name='devmachina', region_name='us-east-1')
-# Any clients created from this session will use credentials
-# from the [dev] section of ~/.aws/credentials.
-# print(session)
+import uuid
 
+env_id = uuid.uuid4()
+print(str(env_id)[:7])
+
+try:
+    os.environ['ACCESS_KEY']
+    os.environ['SECRET_KEY']
+    os.environ['OWNER_ARN']
+    
+except KeyError as e:
+    print('Environment variable ' + e.args[0] + ' not defined')
+    
 client = boto3.client('cloud9', region_name='us-east-1',
-                       aws_access_key_id=os.environ['ACCESS_KEY'], 
+                      aws_access_key_id=os.environ['ACCESS_KEY'], 
                       aws_secret_access_key=os.environ['SECRET_KEY']
                       )
 
 response = client.create_environment_ec2(
-        name='devMachina',
+        name=str(env_id)[:7],
         description='0 env cloud9',
-        clientRequestToken='alfonsoenv1',
+        clientRequestToken=str(env_id)[:7],
         instanceType='t3.micro',
         automaticStopTimeMinutes=20,
-        ownerArn='arn:aws:iam::726747867029:user/alfonso'
+        ownerArn= os.environ('OWNER_ARN')
         )
+
 print(response)
